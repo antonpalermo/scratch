5a2ec0f283079dff80e3f998d5494148438fc3f5
Anton Angelo Palermo <anpbusinessmail@gmail.com>
2024-08-25 22:17:02 +0800

    enable auth prisma adapters.

--- app/api/auth/options.ts
+++ app/api/auth/options.ts
@@ -1,8 +1,12 @@
 import { NextAuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
 
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
+import prisma from "@/libs/prisma";
 
 const options: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT,
