134154afbbcce5f92a2a10235e987fb22f9d3b14
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2024-09-17 23:44:55 +0800

    fix: dashboard ui on scroll

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -17,7 +17,7 @@ export default function MainLayout({ children }: MainLayoutProps) {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
-        <div className="flex h-full max-h-screen flex-col gap-2">
+        <div className="flex sticky top-0 h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/dashboard" className="flex items-center gap-2 font-semibold">
               <Image
@@ -34,7 +34,7 @@ export default function MainLayout({ children }: MainLayoutProps) {
         </div>
       </div>
       <div className="flex flex-col">
-        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+        <header className="flex sticky top-0 bg-white z-50 h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
           <Sheet>
             <SheetTrigger asChild>
               <Button
