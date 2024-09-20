dfb43798efdf724de4cb5a656f91b7de54a05218
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2024-09-20 23:52:32 +0800

    refac: added notes api and update notes schema

---
+++ app/api/notes/route.ts
@@ -0,0 +1,28 @@
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import options from "../auth/options";
+import { NextResponse } from "next/server";
+import { redirect } from "next/navigation";
+
+export async function GET(req: Request) {
+  const session = await getServerSession(options);
+
+  try {
+    const notes = await prisma.notes.findMany({
+      where: {
+        owner: { email: session?.user?.email! }
+      }
+    });
+
+    return NextResponse.json({
+      data: notes,
+      success: true,
+      message: "successfully fetched all available notes."
+    });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      message: "unable to fetch all available notes."
+    });
+  }
+}
