3d89e7e1a1ad3c4b0e25bf9c9b540ee0c6e517fa
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2024-09-29 21:24:07 +0800

    refac: added placeholder extensions.

--- app/(documents)/_components/editor.tsx
+++ app/(documents)/_components/editor.tsx
@@ -1,12 +1,35 @@
 "use client";
 
 import { useEditor, EditorContent } from "@tiptap/react";
+
 import StarterKit from "@tiptap/starter-kit";
+import Document from "@tiptap/extension-document";
+import Placeholder from "@tiptap/extension-placeholder";
+
+const CustomDocument = Document.extend({
+  content: "heading block*"
+});
 
 export default function Editor() {
   const editor = useEditor({
-    extensions: [StarterKit],
-    content: "<p>Hello World! 🌎️</p>",
+    extensions: [
+      CustomDocument,
+      StarterKit.configure({
+        document: false,
+        heading: {
+          levels: [2, 3, 4]
+        }
+      }),
+      Placeholder.configure({
+        placeholder: ({ node }) => {
+          if (node.type.name === "heading") {
+            return "Untitled";
+          }
+
+          return "Can you add some further context?";
+        }
+      })
+    ],
     editorProps: {
       attributes: {
         class:
