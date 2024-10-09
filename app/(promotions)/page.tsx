18b197ed5d1ac7c5a100435c203d31c4b010e6ac
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2024-10-09 22:42:22 +0800

    force user to dashboard if already signed in

--- app/(promotions)/page.tsx
+++ app/(promotions)/page.tsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+
 import Hero from "./_components/hero";
-import Colaborate from "./_components/colaborate";
-import Planning from "./_components/Planing";
 import Footer from "./_components/footer";
+import Planning from "./_components/Planing";
 import Features from "./_components/feature";
+import Colaborate from "./_components/colaborate";
 import CallToActionSignUp from "./_components/call-to-action";
 
-export default function PromotionPage() {
+import options from "@/app/api/auth/options";
+
+export default async function PromotionPage() {
+  const session = await getServerSession(options);
+
+  if (session) {
+    return redirect("/home");
+  }
+
   return (
-    <div className="max-w-6xl mx-auto min-h-screen flex flex-col">
+    <div className="mx-auto flex min-h-screen max-w-6xl flex-col">
       <Hero />
       <Features />
       <Colaborate />
